9aaa1b02ce815e7ce40f8b763f8214b54e6b09ae
Roman <geo.novikov@bk.ru>
2021-06-03 18:14:48 +0300

    addReview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+
+import React from "react";
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import PrivateOffice from "./components/PrivateOffice/PrivateOffice";
+
+
+
+
 function App() {
-  return <div></div>;
+  
+ return (
+ <div className="App">
+<PrivateOffice/>
+</div>
+ )
 }
 
 export default App;
