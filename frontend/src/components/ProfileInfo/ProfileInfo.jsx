3e0d43e5e888f8fe30b59169d8ebfc75f2555f7f
Roman <geo.novikov@bk.ru>
2021-06-09 18:00:35 +0300

    edit,delete icons

--- frontend/src/components/ProfileInfo/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo/ProfileInfo.jsx
@@ -94,7 +94,12 @@ function ProfileInfo() {
           ) : (
             <div className="wrapper">
               {currentUserReview.map((review) => {
-                return <Reviews key={review._id} review={review} />;
+                return (
+                  <div>
+                <Reviews key={review._id} review={review} />
+                
+                </div>
+                )
               })}
             </div>
           )}
